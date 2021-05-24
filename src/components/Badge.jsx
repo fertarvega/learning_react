67bd2d5a27dfb6c6baa0c3da1b18d02d4a79e4ed
fertarvega <fertarvega99@gmail.com>
2021-05-24 09:24:23 -0600

    Making a dynamic component

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -10,29 +10,29 @@ class Badge extends Component {
             <React.Fragment>
                 <div className="Badge m-5 flex-column">
                     <div className="Badge__header">
-                        <img src={HeaderBackground} alt="header_background" />
+                        <img src={this.props.header_picture} alt="header_background" />
                     </div>
                     <div className="Badge__userInfo container mt-5">
                         <div className="Badge__userImage">
-                            <img src={this.props.picture} alt="profile_image" />
+                            <img src={this.props.profile_picture} alt="profile_image" />
                         </div>
                         <div className="container Badge__userInfoDisplay">
-                            <h4 className="text-center mt-3"> Fernando Tarango <i>20</i></h4>
-                            <p className="text-center">Chihuahua city</p>
+                            <h4 className="text-center mt-3">{this.props.name}<i>{this.props.age}</i></h4>
+                            <p className="text-center">{this.props.city}</p>
                         </div>
                     </div>
                     <div className="Badge__info container pt-3">
                         <div className="row">
                             <div className="col">
-                                <h4 className="text-center fw bold">80k</h4>
+                                <h4 className="text-center fw bold">{this.props.followers}</h4>
                                 <p className="text-center">Followers</p>
                             </div>
                             <div className="col">
-                                <h4 className="text-center fw bold">803k</h4>
+                                <h4 className="text-center fw bold">{this.props.likes}</h4>
                                 <p className="text-center">Likes</p>
                             </div>
                             <div className="col">
-                                <h4 className="text-center fw bold">1.5</h4>
+                                <h4 className="text-center fw bold">{this.props.pictures}</h4>
                                 <p className="text-center">Pictures</p>
                             </div>
                         </div>
