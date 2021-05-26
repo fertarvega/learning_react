9d7a0ad9b9e4a405b7c8514fb90d1615e577723c
fertarvega <fertarvega99@gmail.com>
2021-05-25 18:56:30 -0600

    Register form finished

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -4,30 +4,48 @@ import "./Register.css"
 
 class Register extends Component {
 
-    render(){
-        return(
-        <React.Fragment>
-            <div className="RegisterForm__Main d-flex justify-content-center">
-                <div className="RegisterForm m-5 flex-column">
-                    <div className="RegisterForm__icon container">
-                        <img src={"https://i.imgur.com/rlKfyAI.png"} alt="" />
-                    </div>
-                    <form action="" className="container">
-                        <div className="RegisterForm__inputs">
-                            <h5>E-mail:</h5>
-                            <input type="text"/>
-                            <h5>Password:</h5>
-                            <input type="password"/>
+    render() {
+        return (
+            <React.Fragment>
+                <div className="RegisterForm__Main d-flex justify-content-center">
+                    <div className="RegisterForm m-5 flex-column">
+                        <div className="RegisterForm__icon container">
+                            <img src={"https://i.imgur.com/rlKfyAI.png"} alt="" />
                         </div>
-                        <button className="RegisterForm__button">
-                            Register
-                        </button>
-                        <a href="" className="RegisterForm__forgot">Forgot your password?</a>
-                    </form>
+                        <form action="" className="container">
+                            <div className="RegisterForm__inputs">
+                                <div className="row">
+                                    <div className="col">
+                                        <h5>Name:</h5>
+                                        <input type="text" />
+                                        <h5>E-mail:</h5>
+                                        <input type="text" />
+                                        <h5>Password:</h5>
+                                        <input type="password" />
+                                        <h5>Confirm Password:</h5>
+                                        <input type="password" />
+                                    </div>
+                                    <div className="col">
+                                        <h5>Profile Picture URL:</h5>
+                                        <input type="text" />
+                                        <h5>Header Picture URL:</h5>
+                                        <input type="text" />
+                                        <h5>Age:</h5>
+                                        <input type="number" />
+                                        <h5>City:</h5>
+                                        <input type="text" />
+                                    </div>
+                                </div>
+                            </div>
+                            <button className="RegisterForm__button">
+                                Register
+                            </button>
+                                <a href="" className="RegisterForm__forgot">Do you have an account? Click here</a>
+                        </form>
+                    </div>
+                    <Hero h={"88vh"}></Hero>
                 </div>
-                <Hero h={"88vh"}></Hero>
-            </div>
-        </React.Fragment>
+            </React.Fragment>
         );
     }
 }
